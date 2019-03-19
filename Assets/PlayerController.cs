245b96ea7663e09ba93543e6230702f1fbe98dd0
Dukho Choi <dukhochoi@gmail.com>
2019-03-19 23:03:35 +1100

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A)) {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.velocity = new Vector2(rb.velocity.x, 10);
+        }
+
+    }
+}

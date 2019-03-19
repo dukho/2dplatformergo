b0fa82f524f271d92e3941a7f57e697ca7ab8ae7
Dukho Choi <dukhochoi@gmail.com>
2019-03-19 23:36:30 +1100

    Add flipping sprite

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,10 +15,12 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.A)) {
             rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
         }
 
         if (Input.GetKey(KeyCode.D)) {
             rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {

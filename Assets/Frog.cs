cfbc8f1b822c60491bcfba362ad950ef1e7d97ab
Dukho Choi <dukhochoi@gmail.com>
2019-04-09 22:35:02 +1000

    Add enemy jumping animation

--- Assets/Frog.cs
+++ Assets/Frog.cs
@@ -9,6 +9,9 @@ public class Frog : MonoBehaviour {
     [SerializeField] private float jumpLength = 15f;
     [SerializeField] private float jumpHeight = 15f;
     [SerializeField] private LayerMask ground;
+
+    private Animator anim;
+
     private Collider2D coll;
     private Rigidbody2D rb;
 
@@ -18,11 +21,25 @@ public class Frog : MonoBehaviour {
     void Start () {
         coll = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (facingLeft) {
+        if (anim.GetBool("Jumping")) {
+            if (rb.velocity.y < .1) {
+                anim.SetBool("Falling", true);
+                anim.SetBool("Jumping", false);
+            }
+        }
+
+        if (coll.IsTouchingLayers(ground) && anim.GetBool("Falling")) {
+            anim.SetBool("Falling", false);
+        }
+    }
+
+    private void Move() {
+        if (facingLeft) {
             // test to see if we are beyond the leftCap
             if (transform.position.x > leftCap) {
                 // make sure sprite is facing right location, and if it is not, then face the right direction
@@ -34,11 +51,14 @@ public class Frog : MonoBehaviour {
                 if (coll.IsTouchingLayers(ground)) {
                     // jump to the left!
                     rb.velocity = new Vector2(-jumpLength, jumpHeight);
+                    anim.SetBool("Jumping", true);
                 }
-            } else {
+            }
+            else {
                 facingLeft = false;
             }
-        } else {
+        }
+        else {
             // test to see if we are beyond the rightCap
             if (transform.position.x < rightCap) {
                 if (transform.localScale.x != -1) {
@@ -49,11 +69,14 @@ public class Frog : MonoBehaviour {
                 if (coll.IsTouchingLayers(ground)) {
                     // jump to the right!
                     rb.velocity = new Vector2(jumpLength, jumpHeight);
+                    anim.SetBool("Jumping", true);
                 }
             }
             else {
                 facingLeft = true;
             }
         }
-	}
+    }
+
+
 }
